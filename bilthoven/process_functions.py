cda1fd2161d32c145d97971a115d727e94927d48
Konrad Krenzlin <krenzlin@mail.tu-berlin.de>
2014-06-26 17:14:57 +0200

    add process function

--- bilthoven/process_functions.py
+++ bilthoven/process_functions.py
@@ -1,4 +1,4 @@
-from numpy import array
+from numpy import array, zeros, append
 
 
 def block_iterator(data, block_size=1024, hop_size=1024):
@@ -13,3 +13,19 @@ def block_iterator(data, block_size=1024, hop_size=1024):
     while i < len(data):
         yield array(data[i:i+block_size])
         i += hop_size
+
+
+def process(data, transformation, block_size=1024, hop_size=None):
+    """Applies transformation on audio data blockwise."""
+    if not hop_size:
+        hop_size = block_size
+
+    processed_data = array([])
+
+    random_parameter = 0  # TODO random walk
+    previous_block = zeros(block_size)
+    for current_block in block_iterator(data, block_size=block_size, hop_size=hop_size):
+        processed_block = transformation(current_block, previous_block, random_parameter)
+        processed_data = append(processed_data, processed_block)
+
+    return processed_data
--- tests/test_process_functions.py
+++ tests/test_process_functions.py
@@ -8,12 +8,23 @@ def test_block_iterator():
     data = range(10)
     assert array_equal(list(block_iterator(data, 5, 5)), [array([0,1,2,3,4]), array([5,6,7,8,9])])
 
-    result = [array([0,1,2,3,4,5]), array([6,7,8,9])]
-    for ret, res in izip(block_iterator(data, 6, 6), result):
+    result_should = [array([0,1,2,3,4,5]), array([6,7,8,9])]
+    for ret, res in izip(block_iterator(data, 6, 6), result_should):
         assert array_equal(ret, res)
 
-    result = [array([0,1,2,3,4]), array([3,4,5,6,7]), array([6,7,8,9])]
-    for ret, res in izip(block_iterator(data, 5, 3), result):
+    result_should = [array([0,1,2,3,4]), array([3,4,5,6,7]), array([6,7,8,9])]
+    for ret, res in izip(block_iterator(data, 5, 3), result_should):
         assert array_equal(ret, res)
 
+
+
+def test_process():
+    from bilthoven.process_functions import process
+    from bilthoven.transformations import reverse
+
+    data = array(range(10))
+
+    result_should = array([1,0,3,2,5,4,7,6,9,8])
+    result_is = process(data, reverse, block_size=2)
+    assert array_equal(result_is, result_should)
 

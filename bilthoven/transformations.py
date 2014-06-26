30002943ddad9bd51d32c8439a54ac666b06b6d4
Konrad Krenzlin <krenzlin@mail.tu-berlin.de>
2014-06-26 16:24:53 +0200

    add reverse and diff transformations

--- bilthoven/transformations.py
+++ bilthoven/transformations.py
@@ -1,3 +1,13 @@
 def template(current_block, previous_block, random_parameter):
     """This is just a no-op transformation for you to see what interface you should provide."""
     return current_block
+
+
+def reverse(current_block, *args):
+    """Reverses the data of the current block."""
+    return current_block[::-1]
+
+
+def diff(current_block, previous_block, *args):
+    """Substracts the previous block from the current block."""
+    return current_block - previous_block
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,5 +1,25 @@
+from numpy import array
+
 def test_template():
     from bilthoven.transformations import template
 
     assert template([], [], None) == []
     assert template(range(10), [], None) == range(10)
+
+
+def test_reverse():
+    from bilthoven.transformations import reverse
+
+    assert reverse([]) == []
+    assert reverse(range(10)) == list(reversed(range(10)))
+
+    np_array = array(range(10))
+    assert list(reverse(np_array)) == list(reversed(np_array))
+
+
+def test_diff():
+    from bilthoven.transformations import diff
+
+    a = array([1,2,3,4])
+    b = array([1,1,1,1])
+    assert all(diff(a, b) == array([0,1,2,3]))

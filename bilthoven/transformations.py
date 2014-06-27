8431c91ec28c87b70d3f866b1f1a8814704fa0d2
Konrad Krenzlin <konrad>
2014-06-27 15:31:32 +0200

    add reduce transformation

--- bilthoven/transformations.py
+++ bilthoven/transformations.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def template(current_block, previous_block, random_parameter):
     """This is just a no-op transformation for you to see what interface you should provide."""
     return current_block
@@ -11,3 +14,21 @@ def reverse(current_block, *args):
 def diff(current_block, previous_block, *args):
     """Substracts the previous block from the current block."""
     return current_block - previous_block
+
+
+def fft(current_block, *args):
+    F = np.fft.rfft(current_block)
+    f = np.fft.irfft(F).astype(np.int16)
+    return f
+
+
+def reduce(current_block, *args):
+    N = 4
+    F = np.fft.fft(current_block)
+
+    indexes_of_N_max_values = np.imag(F).argsort()[::-1][:N]
+
+    reduced_F = np.zeros(len(F)).astype(np.complex128)
+    reduced_F[indexes_of_N_max_values] = F[indexes_of_N_max_values]
+
+    return np.abs(np.fft.ifft(reduced_F)).astype(np.int16)

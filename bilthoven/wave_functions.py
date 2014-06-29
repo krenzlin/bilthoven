28e0efa836b0e67274aa932f4c36796d74183df3
Konrad Krenzlin <krenzlin@mail.tu-berlin.de>
2014-06-29 14:41:11 +0200

    normalize and convert in read/write_wave

--- bilthoven/wave_functions.py
+++ bilthoven/wave_functions.py
@@ -1,11 +1,15 @@
 import scipy.io.wavfile
+import numpy as np
 
 
 def read_wave(file_name):
     rate, data = scipy.io.wavfile.read(file_name)
+    data /= data.astype(np.float64) / np.iinfo(np.int16).max
     return rate, data
 
 
 def write_wave(file_name, rate, data):
+    data *= np.iinfo(np.int16).max
+    data = data.astype(np.int16)
     scipy.io.wavfile.write(file_name, rate, data)
 

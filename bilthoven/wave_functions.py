9b933b0dfddc2db6a13257eb77ec9c158233e943
Konrad Krenzlin <konrad>
2014-07-02 17:28:50 +0200

    fix: problem when writing more than one channel and these have different length

--- bilthoven/wave_functions.py
+++ bilthoven/wave_functions.py
@@ -10,7 +10,13 @@ def read_wave(file_name):
 
 
 def write_wave(file_name, rate, data):
-    data *= np.iinfo(np.int16).max
+    data *= np.iinfo(np.int16).max - 1
+    if data.dtype == np.object:
+        # need to make every channel the same length
+        new_data = np.zeros((len(max(data, key=len)), data.shape[0]))
+        for i in range(data.shape[0]):
+            new_data[:len(data[i]), i] = data[:][i]
+        data = new_data
     data = data.astype(np.int16)
     scipy.io.wavfile.write(file_name, rate, data)
 
